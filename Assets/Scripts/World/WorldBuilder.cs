65a122b80b75b5bd504902d89ba3db3f008ffc92
= <jake@mcleman.com>
2021-04-23 23:15:26 -0700

    Added ore generation

--- Assets/Scripts/World/WorldBuilder.cs
+++ Assets/Scripts/World/WorldBuilder.cs
@@ -25,22 +25,33 @@ public class WorldBuilder : MonoBehaviour
     {
         terrainTiles = transform.Find("Terrain").GetComponent<Tilemap>();
 
+        
+
         for(int x = -xBounds; x < xBounds; ++x)
         {
-            float samplePos = (x - xBounds) / 40.0f;
+            float heightSamplePos = (x - xBounds) / 40.0f;
 
-            float hilliness = Mathf.PerlinNoise(samplePos, -1.0f);
-            float height = Helpers.Remap(hilliness * Mathf.PerlinNoise(samplePos, 0.0f), 0, 1, -minDepth, -depthLimit);
-            float sandThickness = Helpers.Remap(Mathf.PerlinNoise(samplePos, 0.5f), 0, 1, sandThickAvg + sandThickVariance, sandThickAvg - sandThickVariance);
+            float hilliness = Mathf.PerlinNoise(heightSamplePos, -1.0f);
+            float height = Helpers.Remap(hilliness * Mathf.PerlinNoise(heightSamplePos, 0.0f), 0, 1, -minDepth, -depthLimit);
+            float sandThickness = Helpers.Remap(Mathf.PerlinNoise(heightSamplePos, 0.5f), 0, 1, sandThickAvg + sandThickVariance, sandThickAvg - sandThickVariance);
 
             for(int y = -depthLimit; y < height; ++y)
             {
+                float oreSampleXpos = (x - xBounds) / 35.0f;
+                float oreSampleYpos = y / 20.0f;
+                float ore = Mathf.PerlinNoise(oreSampleXpos, oreSampleYpos);
+                //ore *= ore;
+
                 Tile toPlace = y < (height - sandThickness) ? rockTile : sandTile;
+
+                if(ore > 0.7f && Random.Range(0.7f, 1f) < ore)
+                {
+                    toPlace = goldTile;
+                }
+
                 Vector3Int tilePos = new Vector3Int(x, y, 0);
                 terrainTiles.SetTile(tilePos, toPlace);
             }
-
-            Debug.Log(height);
         }
     }
 }
